b7437194176af4db2ecc8c8309797e861df449f1
Rafael de Figueiredo Lopes <46937945+Rafaelflopes152@users.noreply.github.com>
2026-08-30 14:16:27 -0300

    Add ativaletra function for typing effect

---
+++ script.js
@@ -0,0 +1,10 @@
+function ativaletra(elemento){
+    const arrTexto = elemento.innerHTML.split("");
+    elemento.innerHTML = "";
+    arrTexto.forEach((letra, i) => {
+        setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
+}
+
+const titulo = document.querySelector(".digitando");
+ativaletra(titulo);
